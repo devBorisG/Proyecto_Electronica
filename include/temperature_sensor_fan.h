94c1d9f01b5192a9c265d4819063b9e15b985d83
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-03-25 09:25:33 -0500

    Se configura correctamente la activacion del vetilador con el limite de temperatura

--- include/temperature_sensor_fan.h
+++ include/temperature_sensor_fan.h
@@ -2,23 +2,22 @@
 #include <DHT.h>
 
 int sensor = 2; // Pin de entrada analogo donde estara el sensor de humedad
-int motor = 10; // Pin de entrada digital donde estara el motor (ventilador)
 float temperature = 0; // Variable donde se almacenara el datos de temperatura 
 
 #define DHTPIN sensor          // Seleccionamos el pin en el que se conectará el sensor
 #define DHTTYPE DHT11     // Se selecciona el DHT11 que es el que tenemos
 DHT dht(DHTPIN, DHTTYPE); // Se inicia una variable que será usada por Arduino para comunicarse con el sensor
 
-void activate_fan()
+void activate_fan(int motor)
 {
     temperature = dht.readTemperature(); // Se lee la temperatura
-    // Se imprimen las variables
+    // Se imprime la temperatura en pantalla
     Serial.print("Temperatura: ");
     Serial.print(temperature);
-    Serial.println(" C");
-    delay(2000); // Se espera 2 segundos para seguir leyendo datos
+    Serial.println(" C°");
+    delay(800); // Se espera 2 segundos para seguir leyendo datos
 
-    if(temperature>25){
+    if(temperature>=30){
         digitalWrite(motor, HIGH); //Encender el motor si la temperatura esta por encima de 25°C; se puede cambiar "HIGH" por "1", son equivalentes
     }else{
         digitalWrite(motor, LOW); //El motor se apaga si el sensor analiza temperatura por debado de los 25°c
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include <temperature_sensor_fan.h>
 
+int motor = 3; // Pin de acceso al transistor que controlara el flujo de corriente hacia el motor
+
 void setup()
 {
+  pinMode(motor, OUTPUT);
   dht.begin(); //Se inicia el sensor de temperatura y humedad
   Serial.begin(9600); // Se inicia la comunicación serial  // Se inicia el sensor
 }
 void loop()
 {
-  activate_fan();
+  activate_fan(motor);
 }
