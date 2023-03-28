64cb1a9dd6aea5c71b789fe015dcafe170cdfc6b
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-03-27 19:11:12 -0500

    committed

--- include/door_control.h
+++ include/door_control.h
@@ -3,18 +3,13 @@
 
 void open_door(Servo servoMotor)
 {
-    // Desplazamos a la posición 0º
-    servoMotor.write(0);
-    // Esperamos 1 segundo
-    delay(1000);
-
     // Desplazamos a la posición 90º
     servoMotor.write(90);
     // Esperamos 1 segundo
-    delay(1000);
+    delay(4000);
 
     // Desplazamos a la posición 180º
-    servoMotor.write(180);
+    servoMotor.write(250);
     // Esperamos 1 segundo
-    delay(1000);
+    delay(4000);
 }
